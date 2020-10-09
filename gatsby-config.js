f966d5fca08e1e59b205f0ff5112fe8de1163f09
Oliver Smith-Wellnitz <oliversw.artist@gmail.com>
2020-10-09 18:23:02 +0100

    Minor changes to metadata

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Oliver Smith-Wellnitz' Web Development Portfolio Page`,
+    description: `Oliver Smith-Wellnitz - Professional Circus Artist turned Web Developer`,
+    author: `@oliversw`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
