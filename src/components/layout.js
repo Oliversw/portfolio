a341f39602eea15c6968ce816e9f905e9109a8ce
Oliver Smith-Wellnitz <oliversw.artist@gmail.com>
2020-10-23 18:27:22 +0100

    Remove layout css reset from layout.js

--- src/components/layout.js
+++ src/components/layout.js
@@ -11,7 +11,7 @@ import { useStaticQuery, graphql } from "gatsby"
 import GlobalStyle from "./globalCSS"
 
 import Header from "./header"
-import "./layout.css"
+// import "./layout.css"
 
 const Layout = ({ children }) => {
   const data = useStaticQuery(graphql`
