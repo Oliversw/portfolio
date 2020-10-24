2565489b193d4cf6d2f1f8a7168407c984326bca
Oliver Smith-Wellnitz <oliversw.artist@gmail.com>
2020-10-24 15:02:53 +0100

    Add more styles to landing

--- src/components/header.js
+++ src/components/header.js
@@ -1,8 +1,11 @@
 import React from "react"
 import styled from "styled-components"
+import ChevronDown from "../svg/chevronDown.svg"
 
 const SiteHeader = styled.header`
   min-height: 100vh;
+  display: flex;
+  flex-flow: column nowrap;
 `
 
 const SpanSlight = styled.span`
@@ -13,12 +16,35 @@ const SpanSlight = styled.span`
   font-size: 1.8rem;
 `
 
+const Cap = styled.span`
+  text-transform: uppercase;
+`
+
+const Button = styled.button`
+  background: none;
+  border: none;
+  border-radius: 25rem;
+  outline: none;
+  width: 4rem;
+  height: 4rem;
+  padding: inherit;
+`
+
 const Header = ({ siteTitle }) => (
   <SiteHeader>
-      <h1><SpanSlight>Hi, I'm </SpanSlight>Oliver. <SpanSlight>and I'm a professional circus artist turned </SpanSlight>Software Developer</h1>
-      <h2>Find out more:</h2>
+    <h1>
+      <SpanSlight>
+        <Cap>H</Cap>i, I'm{" "}
+      </SpanSlight>
+      Oliver.{" "}
+      <SpanSlight>and I'm a professional circus artist turned </SpanSlight>
+      Software Developer
+    </h1>
+    <h2>Find out more:</h2>
+    <Button>
+      <img src={ChevronDown} />
+    </Button>
   </SiteHeader>
 )
-
 
 export default Header
